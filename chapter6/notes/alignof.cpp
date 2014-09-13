1a06519387ca3078933f7bb9a09989d89963201e
Samvel Khalatyan <samvel@yandex-team.ru>
2014-09-12 19:12:33 -0700

    Bad vs Good alignment demo

---
+++ chapter6/notes/alignof.cpp
@@ -0,0 +1,42 @@
+// clang++ --std=c++11 -Wpadded alignof.cpp -o alignof
+
+#include <iostream>
+
+using namespace std;
+
+struct TBadAlignment {
+    char a;
+    int *p;
+    char c;
+    int e;
+    short d;
+} Data1;
+
+struct TGoodAlignment {
+    int *p;
+    int e;
+    short d;
+    char a;
+    char c;
+} Data2;
+
+template<typename T>
+void Print(const T& object) {
+    cout << "---" << endl;
+    cout << "    class: " << typeid(T).name() << endl;
+    cout << "alignment: " << alignof(T) << endl;
+    cout << "     size: " << sizeof(object) << endl;
+}
+
+template<typename T1, typename T2>
+void Compare(const T1& obj1, const T2& obj2) {
+    Print(obj1);
+    Print(obj2);
+    cout << "===" << endl;
+    cout << "size reduce: " << 100 - 100 * sizeof(obj2) / sizeof(obj1) << '%' << endl;
+}
+
+int main(int, char* []) {
+    Compare(Data1, Data2);
+    return 0;
+}
