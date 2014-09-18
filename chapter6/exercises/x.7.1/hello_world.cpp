cee0558019a15fbac883563a6a8a6c56569deed8
Samvel Khalatyan <samvel@yandex-team.ru>
2014-09-18 09:47:55 -0700

    Complete exercise x.7.1

---
+++ chapter6/exercises/x.7.1/hello_world.cpp
@@ -0,0 +1,9 @@
+// clang++ --std=c++11 -Wpadded hello_world.cpp -o hello_world
+
+#include <iostream>
+
+int main(int, char*[]) {
+    using namespace std;
+    cout << "hello world!" << endl;
+    return 0;
+}
